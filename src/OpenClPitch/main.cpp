3c8170b8cfec4da29567b92fd848233b01db4eb9
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2020-05-01 09:10:36 +0900

    add empty xcode project

---
+++ src/OpenClPitch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenClPitch
+//
+//  Created by Nobuhiro Kuroiwa on 2020/05/01.
+//  Copyright © 2020 Nobuhiro Kuroiwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
