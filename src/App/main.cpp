c537bb41b1beb66ba769d298833662e205763cf7
hamling-ling <nobubijinesu@gmail.com>
2020-05-07 01:16:22 +0000

    correct naming rule in main

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 
-int16_t conv2int16(float val)
+int16_t Conv2Int16(float val)
 {
     // converting to int16
     return val * 32768;
 }
 
-float conv2float(int16_t val)
+float Conv2Float(int16_t val)
 {
     return ((float)val) / 32768.0f;
 }
 
-void debugPrintAutoCorr(const std::vector<int16_t>& x)
+void DebugPrintAutoCorr(const std::vector<int16_t>& x)
 {
     cout << "right acorr ----" << endl;
     const int N = x.size();
@@ -24,8 +24,8 @@ void debugPrintAutoCorr(const std::vector<int16_t>& x)
         float r = 0.0f;
         float m = 0.0f;
         for(int i = 0; i < N-tau; i++) {
-            float x_i    = conv2float(x[i]);
-            float x_itau = conv2float(x[i+tau]);
+            float x_i    = Conv2Int16(x[i]);
+            float x_itau = Conv2Int16(x[i+tau]);
 
             r += x_i * x_itau;
             m += pow(x_i,2.0f) + pow(x_itau,2.0f);
@@ -35,7 +35,7 @@ void debugPrintAutoCorr(const std::vector<int16_t>& x)
     }
 }
 
-void craeteSineWave(float delta_t, std::vector<int16_t>& x)
+void CraeteSineWave(float delta_t, std::vector<int16_t>& x)
 {
     // frequency f Hz wave is sin(2*pi*f*t)
     // if f=440Hz, func is sin(2*pi*440*t)
@@ -44,10 +44,11 @@ void craeteSineWave(float delta_t, std::vector<int16_t>& x)
     for(int i = 0; i < N; i++) {
         float fval = sin(i * delta_t * 440.0 * 2.0 * M_PI);
         // converting to 
-        x[i]     =  conv2int16(fval);
+        x[i]     =  Conv2Int16(fval);
         //cout << "[" << i << "] = " << h_x[i] << endl;
     }
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -56,7 +57,7 @@ int main(int argc, char *argv[])
     const float DELTA_T       = 1.0f/SAMPLING_RATE;
 
     std::vector<int16_t> x( N);
-    craeteSineWave(DELTA_T, x);
+    CraeteSineWave(DELTA_T, x);
 
     PitchDetector detector(SAMPLING_RATE, N);
     if(!detector.Initialize()) {
